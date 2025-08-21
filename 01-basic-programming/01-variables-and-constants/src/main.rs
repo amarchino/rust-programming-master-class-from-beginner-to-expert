49815e245c4e879023cb3a6e10ec682362715cca
amarchino <marchino.alessandro@gmail.com>
2025-08-21 12:40:08 +0000

    Lesson 11

---
+++ 01-basic-programming/01-variables-and-constants/src/main.rs
@@ -0,0 +1,35 @@
+fn main() {
+    let x: i16 = 10;
+    println!("x is: {x}");
+
+    // Mutability
+    // let y: i32 = 5;
+    // y = 10;
+    let mut _y: i32 = 5;
+    _y = 10;
+
+    // Scope
+    {
+        let _z: i32 = 50;
+    }
+    // let s = _z;
+
+    // Shadowing
+    let t: i32 = 10;
+    let t: i32 = t + 10;
+    println!("t is: {t}");
+
+    let u: i32 = 3;
+    let u: f64 = 3.0;
+    println!("u is: {u}");
+
+    let v: i32 = 30;
+    {
+        let v: i32 = 40;
+        println!("Inner v is: {v}");
+    }
+    println!("v is: {v}");
+
+    // Constants
+    const MAX_VALUE: u32 = 100;
+}
